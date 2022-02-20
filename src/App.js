0b67d91c98257a8544d23e35d6d606848bc5fe5d
Arthur Bernardo Messias <turmessias@gmail.com>
2022-02-20 19:55:44 -0300

    Testando deploy

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <h1> My React Test</h1>
+       <h2> Teste do teste final finalizando o teste testando o que precisa ser testado</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
