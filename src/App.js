4c0a4b404a6e80a7b294b06255b3965b41126a30
Arthur Bernardo Messias <turmessias@gmail.com>
2022-02-20 20:15:06 -0300

    testando dependencias

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
        <h1> My React Test</h1>
        <h2> Teste do teste final finalizando o teste testando o que precisa ser testado</h2>
        <h3> testando commit para eventuais mudanças</h3>
+       <h3> testando dependencias </h3>
       </div>
     );
   }
